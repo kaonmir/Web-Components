f12cf2841c509689581d1cb4c225ae61da610ea3
KAONMIR <40649016+kaonmir@users.noreply.github.com>
2020-01-09 08:48:15 +0900

    Update Hexagon.js

--- Hexagon/Hexagon.js
+++ Hexagon/Hexagon.js
@@ -1,82 +1,75 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 
 class Hexagon extends Component {
     constructor(props) {
         super(props);
-        const length = parseInt(this.props.length);
-        this.state = {
-            top_h: length / 2,
-            top_w: length * Math.sqrt(3) / 2,
-            middle_h: length,
-            middle_w: length * Math.sqrt(3),
 
-            color: this.props.color,
-            isFoot: this.props.orientation.trim() === "foot" ? true : false,
+        let {length, margin, color, orientation} = this.props;
+        this.state = {
+            length: length?length: 30,
+            margin: margin?margin: 3,
+            color: color?color: "#2952a3",
+            orientation: orientation?orientation:"tiptoe",
         }
     }
     render() {
-        const {top_h, top_w, middle_h, middle_w, color, isFoot} = this.state;
-        const {length, margin} = this.props;
-        let top, middle, bottom, box;
+        const props = this.state;
 
-        if(isFoot) {
-            box = {
-                width: `${3/2*length+Math.sqrt(3)/2*margin}px`,
-                float: "left",
-                marginBottom: `${margin}px`,
-            }
-            top = {
-                width: `${this.props.length}px`,
-                borderBottom: `${top_w}px solid ${color}`,
-                borderLeft: `${top_h}px solid transparent`,
-                borderRight: `${top_h}px solid transparent`
-            }
-            bottom = {
-                width: `${this.props.length}px`,
-                borderTop: `${top_w}px solid ${color}`,
-                borderLeft: `${top_h}px solid transparent`,
-                borderRight: `${top_h}px solid transparent`
-            }
-        }
-        else {
-            box = {
-                float: "left",
-                height: `${3/2*length + 3/2/Math.sqrt(3)*margin}px`,
-                marginRight: `${margin}px`,
-            }
-            top = {
-                width: 0,
-                borderBottom: `${top_h}px solid ${color}`,
-                borderLeft: `${top_w}px solid transparent`,
-                borderRight: `${top_w}px solid transparent`
-            }
-            middle = {
-                width: middle_w,
-                height: middle_h,
-                backgroundColor: color
-            }
-            bottom = {
-                width: 0,
-                borderTop: `${top_h}px solid ${color}`,
-                borderLeft: `${top_w}px solid transparent`,
-                borderRight: `${top_w}px solid transparent`
-            }
-        }
         return(
-            <div style={box}>
-                <div style={top}/>
-                <div style={middle}/>
-                <div style={bottom}/>
-            </div>
+            <HexagonBox {...props}>
+                <HexagonTop {...props}/>
+                <HexagonMiddle {...props}/>
+                <HexagonBottom {...props}/>
+            </HexagonBox>
         )
     }
 }
 
-Hexagon.defaultProps = {
-    color: "red",
-    margin: 3,
-    length: 30,
-    orientation: "tiptoe", // foot or tiptoe
-}
-
+const HexagonBox = styled.div`
+    float: left;
+    display: flex;
+    flex-direction: column;
+    ${props => props.orientation==='foot'?`
+        margin-right: -${props.length/2 - 3*props.margin/(2*Math.sqrt(3))}px;
+        margin-bottom: ${props.margin}px;
+    `:`
+        margin-bottom: -${props.length/2 - 3*props.margin/(2*Math.sqrt(3))}px;
+        margin-right: ${props.margin}px;
+    `}
+`;
+const HexagonTop = styled.div`
+    ${props => props.orientation==='foot'?`
+        width: ${props.length}px;
+        border-bottom: ${props.length*Math.sqrt(3)/2}px solid ${props.color};
+        border-left: ${props.length/2}px solid transparent;
+        border-right: ${props.length/2}px solid transparent;
+    `:`
+        width: 0;
+        border-bottom: ${props.length/2}px solid ${props.color};
+        border-left: ${props.length*Math.sqrt(3)/2}px solid transparent;
+        border-right: ${props.length*Math.sqrt(3)/2}px solid transparent;
+    `}
+`;
+const HexagonMiddle = styled.div`
+    ${props => props.orientation==='foot'?`
+    `:`
+        width: ${props.length*Math.sqrt(3)}px;
+        height: ${props.length}px;
+        background-color: ${props.color};
+    `}
+`;
+const HexagonBottom = styled.div`
+    ${props => props.orientation==='foot'?`
+        width: ${props.length}px;
+        border-top: ${props.length*Math.sqrt(3)/2}px solid ${props.color};
+        border-left: ${props.length/2}px solid transparent;
+        border-right: ${props.length/2}px solid transparent;
+    `:`
+        width: 0;
+        border-top: ${props.length/2}px solid ${props.color};
+        border-left: ${props.length*Math.sqrt(3)/2}px solid transparent;
+        border-right: ${props.length*Math.sqrt(3)/2}px solid transparent;
+    `}
+`;
 export default Hexagon;
