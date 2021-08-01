fa213710ab72f03cc52da29b024efd762e01b62d
kaonmir <thswpvm@cau.ac.kr>
2021-08-01 10:07:13 +0900

    refactorign hexagon

--- Hexagon/Hexagon.js
+++ Hexagon/Hexagon.js
@@ -1,75 +1,85 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import styled, { css } from "styled-components";
 
 class Hexagon extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        let {length, margin, color, orientation} = this.props;
-        this.state = {
-            length: length?length: 30,
-            margin: margin?margin: 3,
-            color: color?color: "#2952a3",
-            orientation: orientation?orientation:"tiptoe",
-        }
-    }
-    render() {
-        const props = this.state;
+    let { length, margin, color, orientation } = this.props;
+    this.state = {
+      length: length || 30,
+      margin: margin || 3,
+      color: color || "#2952a3",
+      orientation: orientation || "tiptoe",
+    };
+  }
+  render() {
+    const props = this.state;
 
-        return(
-            <HexagonBox {...props}>
-                <HexagonTop {...props}/>
-                <HexagonMiddle {...props}/>
-                <HexagonBottom {...props}/>
-            </HexagonBox>
-        )
-    }
+    return (
+      <HexagonBox {...props}>
+        <HexagonTop {...props} />
+        <HexagonMiddle {...props} />
+        <HexagonBottom {...props} />
+      </HexagonBox>
+    );
+  }
 }
 
 const HexagonBox = styled.div`
-    float: left;
-    display: flex;
-    flex-direction: column;
-    ${props => props.orientation==='foot'?`
-        margin-right: -${props.length/2 - 3*props.margin/(2*Math.sqrt(3))}px;
-        margin-bottom: ${props.margin}px;
-    `:`
-        margin-bottom: -${props.length/2 - 3*props.margin/(2*Math.sqrt(3))}px;
-        margin-right: ${props.margin}px;
-    `}
+  float: left;
+  display: flex;
+  flex-direction: column;
+  ${({ orientation, length, margin }) =>
+    orientation === "foot"
+      ? css`
+          margin-right: -${length / 2 - (3 * margin) / (2 * Math.sqrt(3))}px;
+          margin-bottom: ${margin}px;
+        `
+      : css`
+          margin-bottom: -${length / 2 - (3 * margin) / (2 * Math.sqrt(3))}px;
+          margin-right: ${margin}px;
+        `}
 `;
 const HexagonTop = styled.div`
-    ${props => props.orientation==='foot'?`
-        width: ${props.length}px;
-        border-bottom: ${props.length*Math.sqrt(3)/2}px solid ${props.color};
-        border-left: ${props.length/2}px solid transparent;
-        border-right: ${props.length/2}px solid transparent;
-    `:`
-        width: 0;
-        border-bottom: ${props.length/2}px solid ${props.color};
-        border-left: ${props.length*Math.sqrt(3)/2}px solid transparent;
-        border-right: ${props.length*Math.sqrt(3)/2}px solid transparent;
-    `}
+  ${({ orientation, length, color }) =>
+    orientation === "foot"
+      ? css`
+          width: ${length * 2}px;
+          border-bottom: ${(length * Math.sqrt(3)) / 2}px solid ${color};
+          border-left: ${length / 2}px solid transparent;
+          border-right: ${length / 2}px solid transparent;
+        `
+      : css`
+          width: 0;
+          border-bottom: ${length / 2}px solid ${color};
+          border-left: ${(length * Math.sqrt(3)) / 2}px solid transparent;
+          border-right: ${(length * Math.sqrt(3)) / 2}px solid transparent;
+        `}
 `;
 const HexagonMiddle = styled.div`
-    ${props => props.orientation==='foot'?`
-    `:`
-        width: ${props.length*Math.sqrt(3)}px;
-        height: ${props.length}px;
-        background-color: ${props.color};
+  ${({ orientation, length, color }) =>
+    orientation !== "foot" &&
+    css`
+      width: ${length * Math.sqrt(3)}px;
+      height: ${length}px;
+      background-color: ${color};
     `}
 `;
 const HexagonBottom = styled.div`
-    ${props => props.orientation==='foot'?`
-        width: ${props.length}px;
-        border-top: ${props.length*Math.sqrt(3)/2}px solid ${props.color};
-        border-left: ${props.length/2}px solid transparent;
-        border-right: ${props.length/2}px solid transparent;
-    `:`
-        width: 0;
-        border-top: ${props.length/2}px solid ${props.color};
-        border-left: ${props.length*Math.sqrt(3)/2}px solid transparent;
-        border-right: ${props.length*Math.sqrt(3)/2}px solid transparent;
-    `}
+  ${({ orientation, length, color }) =>
+    orientation === "foot"
+      ? css`
+          width: ${length * 2}px;
+          border-top: ${(length * Math.sqrt(3)) / 2}px solid ${color};
+          border-left: ${length / 2}px solid transparent;
+          border-right: ${length / 2}px solid transparent;
+        `
+      : css`
+          width: 0;
+          border-top: ${length / 2}px solid ${color};
+          border-left: ${(length * Math.sqrt(3)) / 2}px solid transparent;
+          border-right: ${(length * Math.sqrt(3)) / 2}px solid transparent;
+        `}
 `;
 export default Hexagon;
--- Hexagon/HexagonTable.js
+++ Hexagon/HexagonTable.js
@@ -1,51 +1,93 @@
-import React, { Component } from 'react';
-import Hexagon from './Hexagon';
+import React, { Component } from "react";
+import Hexagon from "./Hexagon";
 
 class HexagonTable extends Component {
-    render() {
-        const {row, col, length, margin, color, orientation} = this.props;
-        let hexagon_row = {}, hexagon_col ={}, box = {};
-        let rows = [], cols = [];
+  render() {
+    const { row, col, length, margin, color, orientation } = this.props;
+    let hexagon_row = {},
+      hexagon_col = {},
+      box = {};
+    let rows = [],
+      cols = [];
 
-        if(orientation === "tiptoe") {
-            hexagon_row = {
-                display: "flex",
-                width: `${Math.sqrt(3)*length/2+margin+col*(Math.sqrt(3)*length+margin)-margin/2}px`
-            }
+    if (orientation === "tiptoe") {
+      hexagon_row = {
+        display: "flex",
+        width: `${
+          (Math.sqrt(3) * length) / 2 +
+          margin +
+          col * (Math.sqrt(3) * length + margin) -
+          margin / 2
+        }px`,
+      };
 
-            for(let i=0; i<col; i++) cols.push(<Hexagon length={length} margin={margin} color={color} orientation={orientation}/>);
-            for(let i=0; i<row; i++) rows.push(<div style={{...hexagon_row, justifyContent: i%2?"flex-end":"flex-start"} }>{cols}</div>);
-            box = {paddingBottom: `${length/2-3/2/Math.sqrt(3)*margin}px`};
-        } 
-        else {
-            hexagon_col = {
-                display: "inline-flex",
-                flexDirection: "column",
-                height: `${(Math.sqrt(3)*length+margin)*row + Math.sqrt(3)*length/2}px`,
-                marginBottom: `-${margin}px`
-            }
-            
-            for(let i=0; i<row; i++) rows.push(<Hexagon length={length} margin={margin} color={color} orientation={orientation}/>);
-            for(let i=0; i<col; i++) cols.push(
-                <div style={{...hexagon_col, justifyContent: i%2?"flex-end":"flex-start",}}>{rows}</div>);
-            box = {display: "flex"}
-        }
+      for (let i = 0; i < col; i++)
+        cols.push(
+          <Hexagon
+            length={length}
+            margin={margin}
+            color={color}
+            orientation={orientation}
+          />
+        );
+      for (let i = 0; i < row; i++)
+        rows.push(
+          <div
+            style={{
+              ...hexagon_row,
+              justifyContent: i % 2 ? "flex-end" : "flex-start",
+            }}
+          >
+            {cols}
+          </div>
+        );
+      box = {
+        paddingBottom: `${length / 2 - (3 / 2 / Math.sqrt(3)) * margin}px`,
+      };
+    } else {
+      hexagon_col = {
+        display: "inline-flex",
+        flexDirection: "column",
+        height: `${
+          (Math.sqrt(3) * length + margin) * row + (Math.sqrt(3) * length) / 2
+        }px`,
+        marginBottom: `-${margin}px`,
+      };
 
-        return(
-            <div style={box}>
-                { orientation==="tiptoe"? rows: cols}
-            </div>
-        )
+      for (let i = 0; i < row; i++)
+        rows.push(
+          <Hexagon
+            length={length}
+            margin={margin}
+            color={color}
+            orientation={orientation}
+          />
+        );
+      for (let i = 0; i < col; i++)
+        cols.push(
+          <div
+            style={{
+              ...hexagon_col,
+              justifyContent: i % 2 ? "flex-end" : "flex-start",
+            }}
+          >
+            {rows}
+          </div>
+        );
+      box = { display: "flex" };
     }
+
+    return <div style={box}>{orientation === "tiptoe" ? rows : cols}</div>;
+  }
 }
 
 HexagonTable.defaultProps = {
-    row: 3,
-    col: 3,
-    length: 30,
-    margin: 3,
-    color: "#2952a3",
-    orientation: "tiptoe" // foot or tiptoe
-}
+  row: 3,
+  col: 3,
+  length: 30,
+  margin: 3,
+  color: "#2952a3",
+  orientation: "tiptoe", // foot or tiptoe
+};
 
 export default HexagonTable;
